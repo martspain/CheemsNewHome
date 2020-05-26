fcd72f8bf99c0ac7ea46d9ce7e873762d61fa62b
Olivverde19270 <oliver.milian00@gmail.com>
2020-05-26 17:34:00 -0600

    Art Concept implemented

---
+++ ProyectoGrupo4/Assets/Scripts/Script Level1/MoveF.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveF : MonoBehaviour
+{
+    private Vector2 iniPos;
+    public int mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iniPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = iniPos + new Vector2(Mathf.Sin(Time.time) * mov, 0);
+    }
+}
---
+++ ProyectoGrupo4/Assets/Scripts/Script Level1/MoveV.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveV : MonoBehaviour
+{
+    private Vector2 iniPos;
+    public float up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iniPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = iniPos + new Vector2(0, Mathf.Sin(Time.time) * up);
+    }
+}
