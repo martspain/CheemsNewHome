a04a6ea4345a32d8a8266b6965170267c291ea8b
lauratamath <laurylevi@hotmail.com>
2020-06-01 07:22:55 -0600

    Se sigue cayendo D:

---
+++ ProyectoGrupo4/Assets/Scripts/Script Level1/HorizonalS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizonalS : MonoBehaviour
+{
+    private Vector2 iniPos;
+    public float ampli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iniPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = iniPos + new Vector2(Mathf.Sin(Time.time) * ampli, 0);
+    }
+}
---
+++ ProyectoGrupo4/Assets/Scripts/Script Level1/VerticalS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalS : MonoBehaviour
+{
+    private Vector2 iniPos;
+    public float ampli;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iniPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = iniPos + new Vector2(0, Mathf.Sin(Time.time) * ampli);
+    }
+}
